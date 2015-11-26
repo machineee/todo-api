860d7bf7e90fa46c0dcdf6115e8396fa6055dc7f
Boban <bobadilla988@gmail.com>
2015-11-26 05:14:33 +0100

    Final static todos

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var todoNextId = 1;
 app.use(bodyParser.json());
 
 
-//GET /todos
+//GET /todos?completed=true&q=work
 app.get('/todos', function (req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -19,6 +19,13 @@ app.get('/todos', function (req, res) {
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
 	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		})
+	}
+
 
 	res.json(filteredTodos);
 });
