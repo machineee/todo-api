e18de75ccd62257d9ccb55cac35d72212b769361
Boban <bobadilla988@gmail.com>
2015-11-26 03:42:54 +0100

    Add DELETE todos route

--- server.js
+++ server.js
@@ -8,21 +8,6 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-// var todos = [{
-// 	id: 1,
-// 	description : 'Meet mom for lunch',
-// 	completed : false
-// },
-// {
-// 	id: 2,
-// 	description : 'Go to market',
-// 	completed : false
-// },
-// {
-// 	id: 3,
-// 	description : 'Do homework',
-// 	completed : true	
-// }];
 
 //GET /todos
 app.get('/todos', function (req, res) {
@@ -64,6 +49,21 @@ app.post('/todos', function (req, res) {
 	console.log('description: '+ body.description);
 
 	res.json(body);
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+
+
 });
 
 app.listen(PORT, function () {
